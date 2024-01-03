af3c065091f3931b2f354075fdd509969baf8b88
Dari <pinobobino@gmail.com>
2024-01-03 14:33:38 +0300

    Final

---
+++ react_photo_slider/src/components/IconButton/IconButton.js
@@ -0,0 +1,11 @@
+import './IconButton.css'
+
+export const IconButton = ({ direction, disable, onClick }) => {
+  let cls = 'icon-button'
+
+  if (direction === 'left') cls += ' left'
+  if (direction === 'right') cls += ' right'
+  if (disable) cls += ' disable, onClick'
+
+  return <div className={cls} onClick={onClick} />
+}
